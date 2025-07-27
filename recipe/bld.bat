d2a3b796bb9ec5abd5aec3914a912c26def0ce9e
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-27 13:54:12 +0200

    change to bash -lc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,6 @@ set "CXXFLAGS=/MD"
 set "LD=link"
 
 
-bash -c "./configure --enable-msvc --prefix=%LIBRARY_PREFIX% --exec-prefix=%LIBRARY_PREFIX% --with-blas-lib='-L${PREFIX}/lib -lblas' --with-lapack-lib='-L${PREFIX}/lib -llapack'"
-bash -c "make -j%CPU_COUNT%"
-bash -c "make install"
+bash -lc "./configure --enable-msvc --prefix=%LIBRARY_PREFIX% --exec-prefix=%LIBRARY_PREFIX% --with-blas-lib='-L${PREFIX}/lib -lblas' --with-lapack-lib='-L${PREFIX}/lib -llapack'"
+bash -lc "make -j%CPU_COUNT%"
+bash -lc "make install"
